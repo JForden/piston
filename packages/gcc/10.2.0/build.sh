c30159dd36a8fe3f9a3a7f65f76dc228b00755fa
JForden <jrfaster@gmail.com>
2024-01-03 15:24:20 -0600

    Java Multifile changes

--- packages/gcc/10.2.0/build.sh
+++ packages/gcc/10.2.0/build.sh
@@ -17,10 +17,15 @@ tar xzf gcc.tar.gz --strip-components=1
 cd ../obj
 
 # === autoconf based === 
-../build/configure --prefix "$PREFIX" --enable-languages=c,c++,d,fortran --disable-multilib --disable-bootstrap
+../build/configure --prefix "$PREFIX" --enable-languages=c,c++,d,java,fortran --disable-multilib --disable-bootstrap
 
 make -j$(nproc)
 make install -j$(nproc)
 cd ../
 rm -rf build obj
+
+curl "https://download.java.net/java/GA/jdk15.0.2/0d1cfde4252546c6931946de8db48ee2/7/GPL/openjdk-15.0.2_linux-x64_bin.tar.gz" -o java.tar.gz
+
+tar xzf java.tar.gz --strip-components=1
+rm java.tar.gz
 
